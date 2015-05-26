89eec6b1a0de9f7fdaf8f251d0807a471ad9e8d4
Trevor SIbanda <trevorsibb@gmail.com>
2015-05-26 12:27:39 +0200

    Create vpayments.php

---
+++ vpayments.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * VPayments API
+ *
+ * @author    Trevor Sibanda <trevorsibb@gmail.com>
+ * @date      26 May 2015
+ *
+ *
+ */
+ 
+ class VPayments 
+ {
+    public function __construct()
+    {
+    
+    }
+ 
+ };
